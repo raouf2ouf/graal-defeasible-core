f80a5380806277148aa74d6b655c24c0711b962b
Raouf Hecham <hecham.abdelraouf@gmail.com>
2019-05-29 15:17:47 +0200

    Updated pom.xml for ssh

--- src/main/java/fr/lirmm/graphik/graal/defeasible/core/Authorable.java
+++ src/main/java/fr/lirmm/graphik/graal/defeasible/core/Authorable.java
@@ -1,8 +1,9 @@
 package fr.lirmm.graphik.graal.defeasible.core;
 
+
 import java.util.HashSet;
 
 public interface Authorable {
-	public HashSet<String> getAuthors();
-	public void setAuthors(HashSet<String> authors);
+	HashSet<String> getAuthors();
+	void setAuthors(HashSet<String> authors);
 }
